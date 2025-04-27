86e10eeb8ee6c0d1f977f3cdbe8b1223b5a8565f
Jefferson <jeffersoncerqueira75@gmail.com>
2025-04-27 13:58:20 -0300

    .

--- lib/app/scale_simulator/scale_simulator_view.dart
+++ lib/app/scale_simulator/scale_simulator_view.dart
@@ -20,33 +20,69 @@ class _ScaleSimulatorViewState extends State<ScaleSimulatorView> {
   @override
   void initState() {
     super.initState();
+    widget.scaleSimulatorViewModel.getAvailableDevices().then((_) {
+      Timer.periodic(Duration(seconds: 1), (_) async {
+        widget.scaleSimulatorViewModel.getAvailableDevices();
+      });
 
-    Timer.periodic(Duration(seconds: 1), (_) {
-      widget.scaleSimulatorViewModel.getAvailableDevices();
+      widget.scaleSimulatorViewModel.loadSamples();
+
+      Timer.periodic(Duration(seconds: 1), (timer) {
+        widget.scaleSimulatorViewModel.setCounter(
+          widget.scaleSimulatorViewModel.counter + 1,
+        );
+
+        if (widget.scaleSimulatorViewModel.counter >= 10) {
+          widget.scaleSimulatorViewModel.generateRandomResult(context);
+          widget.scaleSimulatorViewModel.setCounter(0);
+        }
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: Center(
         child: Stack(
           children: [
             Padding(
               padding: const EdgeInsets.only(left: 24, top: 50.0),
-              child: Image.asset(
-                "assets/labvantage_logo.png",
-                color: onBgColor,
-                width: 200,
+              child: Row(
+                children: [
+                  Image.asset(
+                    "assets/labvantage_logo.png",
+                    color: onBgColor,
+                    width: 400,
+                  ),
+
+                  SizedBox(width: 20),
+                  Visibility(
+                    visible: widget.scaleSimulatorViewModel.isAutomatic,
+                    child: ListenableBuilder(
+                      listenable: widget.scaleSimulatorViewModel,
+                      builder:
+                          (context, _) => Text(
+                            "Running Automatic... ${widget.scaleSimulatorViewModel.counter}",
+
+                            style: TextStyle(
+                              color: Colors.orange,
+                              fontSize: 32,
+                            ),
+                          ),
+                    ),
+                  ),
+                ],
               ),
             ),
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  "Pego Kg",
-                  style: TextStyle(color: onBgColor, fontSize: 18),
+                  "Weight",
+                  style: TextStyle(color: onBgColor, fontSize: 24),
                 ),
                 ListenableBuilder(
                   listenable: widget.scaleSimulatorViewModel,
@@ -71,6 +107,10 @@ class _ScaleSimulatorViewState extends State<ScaleSimulatorView> {
                                           .scaleSimulatorViewModel
                                           .valueController,
                                   textAlign: TextAlign.end,
+                                  onChanged: (value) {
+                                    widget.scaleSimulatorViewModel.value =
+                                        value;
+                                  },
                                   decoration: InputDecoration(
                                     border: InputBorder.none,
                                     enabled:
@@ -89,47 +129,162 @@ class _ScaleSimulatorViewState extends State<ScaleSimulatorView> {
                         ),
                       ),
                 ),
+                SizedBox(height: 10),
+                ListenableBuilder(
+                  listenable: widget.scaleSimulatorViewModel,
+                  builder: (context, _) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        color:
+                            widget.scaleSimulatorViewModel.isOn
+                                ? onBgColor
+                                : const Color.fromARGB(255, 68, 68, 68),
+                        border: Border.all(color: Colors.grey, width: 10),
+                      ),
+                      child: SizedBox(
+                        width: size.width * 0.68,
+                        height: size.height * 0.15,
+
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: DropdownButtonFormField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              enabled: widget.scaleSimulatorViewModel.isOn,
+                            ),
+                            style: TextStyle(fontSize: 32, color: Colors.black),
+
+                            alignment: Alignment.center,
+                            items:
+                                widget
+                                    .scaleSimulatorViewModel
+                                    .samplesCreatedToday
+                                    .map(
+                                      (sample) => DropdownMenuItem(
+                                        value: sample,
+                                        child: Text(
+                                          sample,
+                                          textAlign: TextAlign.end,
+                                          style: TextStyle(color: Colors.black),
+                                        ),
+                                      ),
+                                    )
+                                    .toList(),
+                            value: widget.scaleSimulatorViewModel.sampleId,
+                            onChanged: (value) {
+                              widget.scaleSimulatorViewModel.sampleId = value;
+                            },
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                SizedBox(height: 10),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text("Bal. 1", style: TextStyle(color: onBgColor)),
+                    Text("Min: 10g", style: TextStyle(color: onBgColor)),
                     SizedBox(width: 5),
-                    Text("Max 6 kg", style: TextStyle(color: onBgColor)),
-                    SizedBox(width: 5),
-                    Text("Min 2 g", style: TextStyle(color: onBgColor)),
-                    SizedBox(width: 5),
-                    Text("e=d=2 g", style: TextStyle(color: onBgColor)),
+                    Text("Max: 100g", style: TextStyle(color: onBgColor)),
                   ],
                 ),
                 SizedBox(height: 20),
                 OverflowBar(
                   children: [
-                    FloatingActionButton(
-                      onPressed: () {
-                        widget.scaleSimulatorViewModel.toggleScale();
-                      },
-                      backgroundColor: Colors.red,
-                      child: Text("On/Off", style: TextStyle(color: onBgColor)),
+                    SizedBox(
+                      height: 150,
+                      width: 150,
+                      child: FloatingActionButton(
+                        onPressed: () {
+                          widget.scaleSimulatorViewModel.toggleScale();
+                        },
+                        backgroundColor: Colors.red,
+
+                        child:
+                            widget.scaleSimulatorViewModel.isOn
+                                ? Text(
+                                  "Off",
+                                  style: TextStyle(
+                                    color: onBgColor,
+                                    fontSize: 48,
+                                  ),
+                                )
+                                : Text(
+                                  "On",
+                                  style: TextStyle(
+                                    color: onBgColor,
+                                    fontSize: 48,
+                                  ),
+                                ),
+                      ),
                     ),
                     SizedBox(width: 10),
                     ListenableBuilder(
                       listenable: widget.scaleSimulatorViewModel,
                       builder: (context, _) {
-                        return FloatingActionButton(
-                          onPressed:
-                              widget.scaleSimulatorViewModel.devices.isEmpty
-                                  ? null
-                                  : () {
-                                    widget.scaleSimulatorViewModel.sendValue();
-                                  },
-                          backgroundColor: onBgColor,
-                          child:
-                              widget.scaleSimulatorViewModel.devices.isEmpty
-                                  ? Icon(Icons.lock)
-                                  : Text(
-                                    "Print",
-                                    style: TextStyle(color: bgColor),
-                                  ),
+                        return SizedBox(
+                          height: 150,
+                          width: 150,
+                          child: FloatingActionButton(
+                            onPressed:
+                                (widget
+                                            .scaleSimulatorViewModel
+                                            .devices
+                                            .isEmpty ||
+                                        widget
+                                            .scaleSimulatorViewModel
+                                            .isAutomatic)
+                                    ? null
+                                    : () {
+                                      widget.scaleSimulatorViewModel.sendValue(
+                                        context,
+                                      );
+                                    },
+                            backgroundColor: onBgColor,
+                            child:
+                                (widget
+                                            .scaleSimulatorViewModel
+                                            .devices
+                                            .isEmpty ||
+                                        widget
+                                            .scaleSimulatorViewModel
+                                            .isAutomatic)
+                                    ? Icon(Icons.lock, size: 84)
+                                    : Text(
+                                      "Print",
+                                      style: TextStyle(
+                                        color: bgColor,
+                                        fontSize: 32,
+                                      ),
+                                    ),
+                          ),
+                        );
+                      },
+                    ),
+                    SizedBox(width: 10),
+                    ListenableBuilder(
+                      listenable: widget.scaleSimulatorViewModel,
+                      builder: (context, _) {
+                        return SizedBox(
+                          height: 150,
+                          width: 150,
+                          child: FloatingActionButton(
+                            onPressed:
+                                !widget.scaleSimulatorViewModel.isOn
+                                    ? null
+                                    : () {
+                                      widget.scaleSimulatorViewModel
+                                          .toggleAutomatic();
+                                    },
+                            backgroundColor: onBgColor,
+                            child:
+                                !widget.scaleSimulatorViewModel.isOn
+                                    ? Icon(Icons.lock, size: 84)
+                                    : widget.scaleSimulatorViewModel.isAutomatic
+                                    ? Icon(Icons.stop, size: 84)
+                                    : Icon(Icons.play_arrow, size: 84),
+                          ),
                         );
                       },
                     ),
--- lib/app/scale_simulator/view_model/scale_simulator_view_model.dart
+++ lib/app/scale_simulator/view_model/scale_simulator_view_model.dart
@@ -1,79 +1,169 @@
+import 'dart:async';
 import 'dart:typed_data';
-
+import "dart:math" as math;
+import 'package:equipment_simulator_v2/app/scale_simulator/services/interface/sample_service.dart';
+import 'package:equipment_simulator_v2/app/scale_simulator/services/sample_service_impl.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:usb_serial/usb_serial.dart';
 
 class ScaleSimulatorViewModel extends ChangeNotifier {
+  final SampleService sampleService = SampleServiceImpl();
   bool _isOn = false;
 
   bool get isOn => _isOn;
 
-  String _value = "";
+  String value = "";
+  String? sampleId;
 
-  set value(String value) => _value = value;
+  bool isAutomatic = false;
+
+  int counter = 0;
 
   final List<UsbDevice> devices = [];
+
+  final List<String> samplesCreatedToday = [];
 
   TextEditingController valueController = TextEditingController();
 
   void resetValues() {
     _isOn = false;
-    _value = "";
-    valueController.text = _value;
+    value = "";
+    valueController.text = value;
+    notifyListeners();
+  }
+
+  void setCounter(int number) {
+    counter = number;
+    notifyListeners();
+    print(counter);
+  }
+
+  void loadSamples() async {
+    samplesCreatedToday.clear();
+
+    samplesCreatedToday.addAll(await sampleService.getSamplesCreatedToday());
     notifyListeners();
   }
 
   void toggleScale() {
     _isOn = !_isOn;
-    valueController.text = _value;
+    value = "";
+    valueController.text = value;
 
     notifyListeners();
   }
 
-  void getAvailableDevices() async {
+  void toggleAutomatic() {
+    isAutomatic = !isAutomatic;
+    notifyListeners();
+  }
+
+  Future<void> getAvailableDevices() async {
     this.devices.clear();
     List<UsbDevice> devices = await UsbSerial.listDevices();
-
-    for (var device in devices) {
-      print(device.vid);
-      print(device.pid);
-      print(device.productName);
-      print(device.deviceId);
-    }
 
     this.devices.addAll(devices);
     notifyListeners();
   }
 
-  void sendValue() async {
-    if (devices.isEmpty) return;
+  void sendValue(BuildContext context) async {
+    if (devices.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Connect one device to send weighing"),
+          backgroundColor: Colors.red,
+        ),
+      );
 
-    if (_value == "") return;
-
-    UsbDevice device = devices.first;
-
-    UsbPort? port = await device.create();
-
-    bool openResult = await port!.open();
-
-    if (!openResult) {
-      print("Não foi possível abrir a porta.");
+      notifyListeners();
       return;
     }
 
-    await port.setDTR(true);
-    await port.setRTS(true);
-    await port.setPortParameters(
-      115200, // baudRate
-      UsbPort.DATABITS_8,
-      UsbPort.STOPBITS_1,
-      UsbPort.PARITY_NONE,
-    );
+    if (value != "" && sampleId != null && sampleId!.isNotEmpty) {
+      if (devices.isEmpty) {
+        notifyListeners();
+        return;
+      }
 
-    await port.write(Uint8List.fromList(_value.codeUnits));
-    print("Comando enviado");
-    await port.close();
+      UsbDevice device = devices.first;
+
+      UsbPort? port = await device.create();
+
+      if (port == null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Problems with the USB Port"),
+            backgroundColor: Colors.red,
+          ),
+        );
+        notifyListeners();
+        return;
+      }
+
+      bool openResult = await port.open();
+
+      if (!openResult) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Problems with the USB Port"),
+            backgroundColor: Colors.red,
+          ),
+        );
+        notifyListeners();
+      }
+
+      await port.setPortParameters(
+        115200, // baudRate
+        UsbPort.DATABITS_8,
+        UsbPort.STOPBITS_1,
+        UsbPort.PARITY_NONE,
+      );
+
+      await port.write(Uint8List.fromList(value.codeUnits));
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Weighing sent to print!"),
+          backgroundColor: Colors.green,
+        ),
+      );
+      notifyListeners();
+      await port.close();
+
+      await _sendSampleWeightToLims();
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Weighing or Sample ID are empty!"),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  }
+
+  Future<void> _sendSampleWeightToLims() async {
+    if (sampleId == null) {
+      return;
+    }
+    await sampleService.sendSampleWeight(sampleId ?? "", value);
+    await sampleService.sendSampleWeightTAGO(sampleId ?? "", value);
+  }
+
+  Future<void> generateRandomResult(BuildContext context) async {
+    if (!(isOn && isAutomatic)) {
+      return;
+    }
+
+    math.Random random = math.Random();
+
+    String randomValue = (10 + (random.nextDouble() * (100 - 10)))
+        .toStringAsFixed(2);
+
+    valueController.text = randomValue;
+    value = randomValue;
+
+    sendValue(context);
   }
 }
